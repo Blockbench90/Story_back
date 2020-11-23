67ebf84d779b99269f321c0b126fdd30b1ba5308
Ivan Lit <litvin.ivan.ak@gmail.com>
2020-11-23 22:30:32 +0200

    som refactor

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,7 @@
 import {UserModel} from "../models/UserModel";
 import {validationResult} from 'express-validator'
 import {generateMD5} from "../utils/generateHash";
+
 const express = require('express')
 const jwt = require('jsonwebtoken')
 
@@ -20,17 +21,18 @@ class UserController {
             })
         }
     }
+
     async show(req, res) {
         try {
             const userId = req.params.id
 
-            if(!isValidObjectId(userId)){
+            if (!isValidObjectId(userId)) {
                 res.status(400).send()
                 return
             }
             const user = await UserModel.findById(userId).exec()
 
-            if(!user) {
+            if (!user) {
                 res.status(404).send()
                 return
             }
@@ -46,11 +48,12 @@ class UserController {
             })
         }
     }
+
     async create(req, res) {
         try {
             const errors = validationResult(req)
-            if(!errors.isEmpty()) {
-                res.status(400).json({status: 'error', errors: errors.array() })
+            if (!errors.isEmpty()) {
+                res.status(400).json({status: 'error', errors: errors.array()})
                 return
             }
             const data = {
@@ -63,10 +66,96 @@ class UserController {
 
             const user = await UserModel.create(data)
 
-            sendEmail()
+            sendEmail({
+                    emailFrom: 'admin@story.com',
+                    emailTo: data.email,
+                    subject: 'Подтверждение почты',
+                    html: `Для того, чтобы подтвердить почту, перейдите на <a href="http://localhost:${
+                        process.env.PORT || 8888
+                    }/auth/varufy?hash=${data.confirmHash}">по этой ссылке</a>`
+                },
+                (err) => {
+                    if (err) {
+                        res.status(500).json({
+                            status: 'error',
+                            message: err
+                        })
+                    } else {
+                        res.status(201).json({
+                            status: 'success',
+                            data: user
+                        })
+                    }
+                })
 
         } catch (error) {
-            
+            res.status(500).json({
+                status: 'error',
+                message: error
+            })
+        }
+    }
+    async verify(req,res) {
+        try{
+            const hash = req.query.hash
+
+            if(!hash) {
+                res.status(400).send()
+                return
+            }
+            const user = await UserModel.findOne({confirmHash: hash}).exec()
+
+            if(user) {
+                user.confirmed = true
+                await user.save()
+
+                res.json({
+                    status: 'success',
+                    data: user.toJSON()
+                })
+            } else {
+                res.status(404).json({status: 'error', message: 'Пользователь не найден'})
+            }
+        } catch (error) {
+            res.status(500).json({
+                status: 'error',
+                message: error
+            })
+        }
+    }
+    async afterLogin(req, res) {
+        try{
+            const user = req.user ? req.user.toJSON() : undefined
+            res.json({
+                status: 'success',
+                data: {
+                    ...user,
+                    token: jwt.sign({ data: req.user},
+                        process.env.SECRET_KEY || '123', {
+                        expiresIn: '30 days'
+                        })
+                }
+            })
+        } catch (error) {
+            res.status(500).json({
+                status: 'error',
+                message: error
+            })
+        }
+    }
+    async getUserInfo(req, res) {
+        try{
+            const user = req.user ? req.user.toJSON() : undefined
+            res.json({
+                status: 'success',
+                data: user
+            })
+        } catch (error) {
+            res.status(500).json({
+                status: 'error',
+                message: error
+            })
         }
     }
 }
+export const UserCtrl = new UserController()
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const passport = require('passport')
-
+const { UserCtrl } = require('./controllers/UserController')
 const app = express()
 
 app.use(express.json())
 app.use(passport.initialize())
 
-app.get('/users', )
+app.get('/users', UserCtrl.index)
 
 app.listen(3001, () => {
     console.log('SERVER START')
