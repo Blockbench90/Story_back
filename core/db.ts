92b584ebd533eac6b6a9abf09606b17a4244e07e
Blockbench90 <prog.hak.ak@gmail.com>
2020-12-01 18:55:25 +0200

    Начал прикручивать бек к фронту

--- core/db.ts
+++ core/db.ts
@@ -7,7 +7,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/story_bac
   useCreateIndex: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-}).then(()=> console.log("DataBase is started"))
+}).then(()=> console.log("DataBase conected"))
 
 const db = mongoose.connection;
 
